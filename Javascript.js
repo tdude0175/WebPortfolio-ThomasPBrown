0a40431d51396c3393055b5d2839d50dafedbed4
tdude0175 <tdude0175@gmail.com>
2019-02-13 15:42:06 -0600

    Added transition for projects page, fixed minor h5 bug.

--- Javascript.js
+++ Javascript.js
@@ -4,12 +4,26 @@ function giveThemPoints(e)
     e.target.style.display = "none";
 }
 
+function showThing()
+{
+    gameFacts.style.display = "block"
+}
+
+function hideThing()
+{
+    gameFacts.style.display = "none";
+}
+
 var score = document.getElementById("points");
 var starsForGame = document.getElementsByClassName("star");
-
-console.log(starsForGame[0]);
+var gameProject = document.getElementById("gameProject");
+var gameFacts = document.getElementById("gameFacts");
 
 for(let i=0; i < starsForGame.length; i++)
 {
     starsForGame[i].addEventListener("click", giveThemPoints)
 }
+
+gameProject.addEventListener("mouseover", showThing);
+
+gameProject.addEventListener("mouseout",hideThing);
